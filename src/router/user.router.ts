5e0fa6b4e27181a421f2d19bcce578935bb10f3a
Jesus Alexis <ingjesusalexis@gmail.com>
2022-08-14 21:32:30 -0400

    added router

---
+++ src/router/user.router.ts
@@ -0,0 +1,11 @@
+import { UserController } from "../controller/user.controller";
+import { BaseRouter } from "./router";
+
+export class UserRouter extends BaseRouter<UserController>{
+  constructor() {
+    super(UserController)
+  }
+  routes(): void {
+    this.router.get('/user', (req, res) => this.controller.getUser(req, res))
+  }
+}
